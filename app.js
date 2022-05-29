b894e3a64df7df4dac345d8fdc3f56e73106c9ce
uzma <92790017+uzma-nazim@users.noreply.github.com>
2022-05-29 20:27:02 +0500

    complete project

--- app.js
+++ app.js
@@ -5,23 +5,7 @@ const ToggelON = () => {
 
     let mobilemenu = document.querySelector(".mobile-menu")
 
-    // let isShow = true
-    
-    
-    // if (isShow) {
-    //     mobilemenu.style.visibility = "visible"
-    //     mobilemenu.style.transform = " scale(1)"
-    //     isShow = false
-    // }
-    // else{
-    //     console.log("hit");
-    //     mobilemenu.style.visibility = "hidden"
-    //     mobilemenu.style.transform = " scale(0.1)"
 
-    //     isShow = true
-
-    // }
-    
 
     if (mobilemenu.style.visibility === "hidden") {
         mobilemenu.style.visibility = "visible"
@@ -37,3 +21,104 @@ const ToggelON = () => {
 
 
 }
+
+const withDrawbtn = document.getElementById("withDraw")
+const depositebtn = document.getElementById("deposite")
+const buy = document.getElementById("buy")
+const sell = document.getElementById("sell")
+
+const deposite = ()=>{
+    withDrawbtn.style.display= "none"
+    depositebtn.style.display= "block"
+    console.log("HIt");
+
+}
+
+const withDraw = ()=>{
+    withDrawbtn.style.display= "block"
+    depositebtn.style.display= "none"
+
+}
+
+const buyShow = ()=>{
+    buy.style.display= "block"
+    sell.style.display= "none"
+    console.log("HIt");
+
+}
+
+const sellShow = ()=>{
+    buy.style.display= "none"
+    sell.style.display= "block"
+    console.log("HIt");
+
+}
+
+
+const modalOpen = ()=>{
+    alert("hello")
+    console.log("sas");
+
+}
+
+
+const hour = document.getElementById("hour")
+const min = document.getElementById("min")
+const sec = document.getElementById("sec")
+
+
+
+
+
+
+// var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+// // Update the count down every 1 second
+// var x = setInterval(function() {
+
+//   // Get today's date and time
+//   var now = new Date().getTime();
+
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
+
+//   // Time calculations for days, hours, minutes and seconds
+// //   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//   // Display the result in the element with id="demo"
+//   document.getElementById("timer").innerHTML =  hours + "h "
+//   + minutes + "m " + seconds + "s ";
+
+//   // If the count down is finished, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("timer").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
+
+
+
+
+var date;
+var display = document.getElementById('timer');
+setInterval(function(){
+    date = new Date();
+    var currenthours = date.getHours();
+    var hours;
+    var minutes;
+    var secondes;
+    if (currenthours != 23){
+        if (currenthours < 23)
+            hours = 23 - currenthours;
+        else hours = 23 + (23 - currenthours);
+        minutes = 60 - date.getMinutes();
+        secondes = 60 - date.getSeconds();
+    display.innerHTML = hours + 'h: ' + minutes + 'm: ' +secondes + 's';
+    }
+    else display.innerHTML = 'LIVE NOW';
+
+
+},1000);
